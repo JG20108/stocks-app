69bc39cab1acd739a58bec8e5a22df3abc3ce434
JG20108 <joseosgui@hotmail.com>
2024-03-10 23:28:59 -0600

    Populate dropdown based on existing stocks coming from API

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -88,7 +88,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="stock-info">
       <div className="stock-selection-form">
-        <StockSelectionForm onSubmit={handleSubmit} stockOptions={stockOptions} />
+        <StockSelectionForm onSubmit={handleSubmit} stockOptions={stocks.map(stock => stock.name)} />
       </div>
       <div className="stock-display">
         <div className="stock-cards-container">
